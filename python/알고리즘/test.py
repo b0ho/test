1b4e08a1afd664490a9c00757f01dea7bc1435b7
b0ho <ybg4828@gmail.com>
2019-03-29 15:48:14 +0900

    보물상자_완

--- python/알고리즘/test.py
+++ python/알고리즘/test.py
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-n = "XYZT"
-tmp = "AAAA"
-loc = 2
+num = int(input())
 
-tmp = tmp.replace(tmp[loc], n[loc], 1)
-print(tmp)
-=======
-record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo",
-          "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"]
+num_arr = list(map(int, list(str(num))))
 
-tmp = record[0].split()
+sum = 0
+for i in num_arr:
+    sum += i
 
-tt = tmp[0]
-
-print(tt is "Enter")
->>>>>>> 6565de89a0d21a64bca6d83731c87d7c3d38339a
+print(sum)
--- python/알고리즘/보물상자_비밀번호.py
+++ python/알고리즘/보물상자_비밀번호.py
@@ -10,41 +10,38 @@ for test_case in range(1, T + 1):
     answer = 0
     leng = int(N / 4)
 
-    #print("최초 후보", arr)
+    #print("최초 후보", arr, leng)
 
-    for i in range(4):
+    for i in range(leng):
 
-        #후보에 추가
-        #print(arr[0:leng])
-        cand.append(arr[0:leng])
+        for r in range(4):
+            # 후보에 추가
+            # print(arr[leng*r:leng*(r+1)])
+            cand.append(arr[leng*r:leng*(r+1)])
 
-        #print(arr[leng:leng*2])
-        cand.append(arr[leng:leng*2])
+            #print("후보 추가 완료", cand)
 
-        #print(arr[leng*2:leng*3])
-        cand.append(arr[leng*2:leng*3])
-        
-        #print(arr[leng*3:])
-        cand.append(arr[leng*3:])
-
-        #print("후보 추가 완료", cand)
-        
-        #회전
-        #print(arr[1:])
-        #print(arr[0:1])
+        # 회전
+        # print(arr[1:])
+        # print(arr[0:1])
         arr = arr[1:]+arr[0:1]
+
         #print("회전완료", arr)
 
-    #중복 제거
+    # 중복 제거
     cand = list(set(cand))
     #print("중복 제거 완료", cand)
 
-    #후보 정렬
-    cand.sort()
+    # 후보 정렬
+    cand.sort(reverse=True)
     #print("정렬 완료", cand)
 
-    #k번째 배열을 answer로
-    answer = cand[K-1]
+    # print(K)
+    # for i in cand:
+    #print(int(i, 16))
+
+    # k번째 배열을 10진수로
+    answer = int(cand[K-1], 16)
 
     print("#" + str(test_case) + " " + str(answer))
-    #print()
+    # print()
