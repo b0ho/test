b50c17d4c511aa2b37d6e3fb2f6a9f0255a44c20
YOU <b0ho@YOUui-MacBookPro.local>
2020-02-12 02:53:12 +0900

    더맵게

--- java/solution/더맵게.java
+++ java/solution/더맵게.java
@@ -1,1 +1,45 @@
 package test.java.solution;
+
+import java.util.PriorityQueue;
+
+class Solution {
+    public static void main(String args[]) {
+        Solution s = new Solution();
+        int[] scoville = { 1, 2, 3, 9, 10, 12 };
+        int K = 7;
+        System.out.println(s.solution(scoville, K));
+    }
+
+    // 오름차순 정렬 한다.
+    // 지수 공식에 대입한다.
+    // 위를 반복하다 k이상이 되면 종료하고 cnt 리턴
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+
+        PriorityQueue<Integer> scovilles = new PriorityQueue(scoville.length);
+        for (int i : scoville) {
+            scovilles.offer(i);
+        }
+
+        while (scovilles.peek() <= K) {
+            // 섞을수가 없으면 (사이즈 = 1) 종료 : -1
+            if (scovilles.size() < 2) {
+                answer = -1;
+                return answer;
+            }
+
+            answer = answer + 1;
+
+            int first = scovilles.poll();
+            int second = scovilles.poll();
+
+            // 지수 공식
+            int tmp = first + (second * 2);
+
+            scovilles.offer(tmp);
+
+        }
+        return answer;
+    }
+
+}
--- java/solution/큰수만들기.java
+++ java/solution/큰수만들기.java
@@ -1,1 +1,77 @@
 package test.java.solution;
+
+import java.util.*;
+
+class Solution {
+    public static void main(String args[]) {
+        Solution s = new Solution();
+        String number = "1231234";
+        int k = 3;
+        System.out.println(s.solution(number, k));
+    }
+
+    // public String answer;
+    // public ArrayList<Character[]> result;
+    // public Character[] res;
+    // int max = 0;
+
+    // 뺄것을 찾는게 아니라
+    // 시작할 수 있는 가장 큰 수를 찾기
+    public String solution(String number, int k) {
+        StringBuilder answer = new StringBuilder();
+
+        int max = 0;
+        int len = number.length();
+        int how = len - k;
+        int now = 0;
+
+        for (int i = 0; i < how; i++) {
+            max = 0;
+            for (int j = now; j < k + i + 1; j++) {
+                if (max < number.charAt(j) - 48) {
+                    max = number.charAt(j) - 48;
+                    now = j + 1;
+                }
+
+            }
+            answer.append(max);
+        }
+        System.out.println(answer);
+
+        // char[] num_arr = number.toCharArray();
+        // int len = num_arr.length;
+        // int how = len - k;
+        // ArrayList<Character> item = new ArrayList<Character>();
+        // result = new ArrayList<Character[]>();
+        // res = new Character[how];
+        // for (int i = 0; i < num_arr.length; i++) {
+        // item.add(num_arr[i]);
+        // }
+
+        // p(item, 0, len, how);
+
+        return answer.toString();
+    }
+
+    // public void p(ArrayList<Character> item, int now, int len, int how) {
+    // if (now == how) {
+
+    // String tmp = "";
+    // for (int i = 0; i < how; i++) {
+    // tmp += res[i];
+    // }
+
+    // int num = Integer.parseInt(tmp);
+    // if (num > max) {
+    // max = num;
+    // answer = "" + num;
+    // }
+    // return;
+    // }
+    // for (int i = 0; i < len - now; i++) {
+    // res[now] = item.remove(i);
+    // p(item, now + 1, len, how);
+    // item.add(i, res[now]);
+    // }
+    // }
+}
