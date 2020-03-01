b3581f20dc4ce44154432103533c470489ce45b7
b0ho <b0ho@218.38.137.28>
2020-03-02 06:16:02 +0900

    이클립스세

---
+++ java/solution/몸짱트레이너.java
@@ -0,0 +1,9 @@
+
+public class 몸짱트레이너 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java/solution/사이클제거.java
@@ -0,0 +1,9 @@
+
+public class 사이클제거 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
